4214affcf6211ace18ea3247f4498364218bf4d5
Matt Russell <MattRussellUK@gmail.com>
2016-07-05 22:07:23 +0100

    Update sha

--- mash.rb
+++ mash.rb
@@ -3,7 +3,7 @@ class Mash < Formula
   homepage "http://mash-shell.org"
   url "https://github.com/mdr/mash/releases/download/v0.0.2/mash-assembly-0.0.2.jar"
   version "0.0.2"
-  sha256 "b558f78146d43b108f8548d5c9321712918fc63396ecc4117bf8787e16db6a1"
+  sha256 "eb558f78146d43b108f8548d5c9321712918fc63396ecc4117bf8787e16db6a1"
 
   def install
       inreplace "brew/mash", "##PREFIX##", "#{prefix}"
