335c4ffada96c6b735e876d4159e8168ddf41e6e
Matt Russell <MattRussellUK@gmail.com>
2016-07-05 22:38:31 +0100

    wip

--- mash.rb
+++ mash.rb
@@ -5,9 +5,15 @@ class Mash < Formula
   version "0.0.2"
   sha256 "eb558f78146d43b108f8548d5c9321712918fc63396ecc4117bf8787e16db6a1"
 
+  depends_on :java => "1.7+"
+
+  bottle :unneeded
+
   def install
-      inreplace "brew/mash", "##PREFIX##", "#{prefix}"
+      (bin/"sbt").write <<-EOS.undent
+        #!/bin/sh
+        java -jar #{prefix}/mash-assembly-0.0.2.jar
+      EOS
       prefix.install "mash-assembly-0.0.2.jar"
-      bin.install "brew/mash"
   end
 end
