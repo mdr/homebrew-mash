c36420592ad57703c0f6d5f546d58b83b0740522
Matt Russell <MattRussellUK@gmail.com>
2016-07-26 22:05:24 +0100

    fix sha

--- mash.rb
+++ mash.rb
@@ -3,7 +3,7 @@ class Mash < Formula
   homepage "http://mash-shell.org"
   url "https://github.com/mdr/mash/releases/download/v0.0.4/mash-0.0.4.jar"
   version "0.0.4"
-  sha256 "823e5526285a6f19c171d771a0d5d51dd41da28340598734231c34782c874f4c"
+  sha256 "24a66f84d639e3805766092bfe1cf0c8f268b6b09f295616bbcc48b1ca597df6"
   depends_on :java => "1.7+"
 
   bottle :unneeded
