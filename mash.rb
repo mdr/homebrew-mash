6447cbee7b8ed09f8e82aa845ecb7d9c82e2f372
Matt Russell <MattRussellUK@gmail.com>
2017-02-08 22:05:45 +0000

    Bump version

--- mash.rb
+++ mash.rb
@@ -3,7 +3,7 @@ class Mash < Formula
   homepage "http://mash-shell.org"
   url "https://github.com/mdr/mash/releases/download/v0.0.4/mash-0.0.4.jar"
   version "0.0.4"
-  sha256 "24a66f84d639e3805766092bfe1cf0c8f268b6b09f295616bbcc48b1ca597df6"
+  sha256 "a90150aceb92e7a4d7218e78711d3bc58d9344558dd0559fa0c4a5f989161875"
   depends_on :java => "1.7+"
 
   bottle :unneeded
@@ -11,8 +11,8 @@ class Mash < Formula
   def install
       (bin/"mash").write <<-EOS.undent
         #!/bin/sh
-        java -jar #{prefix}/mash-0.0.4.jar
+        java -jar #{prefix}/mash-0.0.5.jar
       EOS
-      prefix.install "mash-0.0.4.jar"
+      prefix.install "mash-0.0.5.jar"
   end
 end
