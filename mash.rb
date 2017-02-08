ab874f008d5cb3203b7c71d9f0e3ff4cc5d5b451
Matt Russell <MattRussellUK@gmail.com>
2017-02-08 22:11:11 +0000

    fix version

--- mash.rb
+++ mash.rb
@@ -1,7 +1,7 @@
 class Mash < Formula
   desc "An object shell for Unix"
   homepage "http://mash-shell.org"
-  url "https://github.com/mdr/mash/releases/download/v0.0.4/mash-0.0.5.jar"
+  url "https://github.com/mdr/mash/releases/download/v0.0.5/mash-0.0.5.jar"
   version "0.0.5"
   sha256 "a90150aceb92e7a4d7218e78711d3bc58d9344558dd0559fa0c4a5f989161875"
   depends_on :java => "1.7+"
