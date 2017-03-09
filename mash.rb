d90bc6cf17037cc7b00b23ef65b984c2e2dd5d5c
Matt Russell <MattRussellUK@gmail.com>
2017-03-09 23:48:03 +0000

    0.0.7

--- mash.rb
+++ mash.rb
@@ -1,9 +1,9 @@
 class Mash < Formula
   desc "An object shell for Unix"
   homepage "http://mash-shell.org"
-  url "https://github.com/mdr/mash/releases/download/v0.0.6/mash-0.0.6.jar"
-  version "0.0.6"
-  sha256 "d5de7ad28909f0a701482227f29f3371d7ef70d4ec42eded9520f0d45810558b"
+  url "https://github.com/mdr/mash/releases/download/v0.0.7/mash-0.0.7.jar"
+  version "0.0.7"
+  sha256 "9bb5f2724df2b66265e9374feac35b97e1dc4bd3386f59af8c0b773178541a6e"
   depends_on :java => "1.7+"
 
   bottle :unneeded
@@ -11,8 +11,8 @@ class Mash < Formula
   def install
       (bin/"mash").write <<-EOS.undent
         #!/bin/sh
-        java -jar #{prefix}/mash-0.0.6.jar
+        java -jar #{prefix}/mash-0.0.7.jar
       EOS
-      prefix.install "mash-0.0.6.jar"
+      prefix.install "mash-0.0.7.jar"
   end
 end
